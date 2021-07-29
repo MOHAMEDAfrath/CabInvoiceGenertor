b287f6d49e7b9520333aa24938d0c4b3ebe358c6
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-29 15:00:31 +0530

    [Afrath]Updated the file to calculate fare for normal ride

---
+++ InvoiceGeneration/InvoiceCustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGeneration
+{
+    public class InvoiceCustomException:Exception
+    {
+        public string message;
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE, INVALID_TIME
+        }
+        public ExceptionType exceptionType;
+        public InvoiceCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.exceptionType = type;
+        }
+    }
+}
